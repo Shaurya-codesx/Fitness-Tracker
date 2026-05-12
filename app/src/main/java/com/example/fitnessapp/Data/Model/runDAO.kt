787b37e76842c1ebba7cddf4819714616f7deab2
Shaurya Sharma <shaurya.shrma7@gmail.com>
2026-05-12 22:18:55 +0530

    dao changes

--- app/src/main/java/com/example/fitnessapp/Data/Model/runDAO.kt
+++ app/src/main/java/com/example/fitnessapp/Data/Model/runDAO.kt
@@ -19,12 +19,11 @@ interface runDAO {
     @Query("SELECT * FROM runs ORDER BY startTime DESC")
     fun getAllRuns() : Flow<List<RunEntity>>
 
+    @Query("SELECT * FROM runs WHERE id = :id")
+    fun getRunById(id : Long) : Flow<RunEntity>
 
     @Query("SELECT * FROM runs WHERE startTime BETWEEN :startTime AND :endTime ORDER BY startTime DESC")
     fun getRunsInRange(startTime : Long, endTime : Long) : Flow<List<RunEntity>>
-
-    @Query("SELECT * FROM runs WHERE id = :id")
-    fun getRunById(id : Long) : Flow<RunEntity>
 
     @Query("SELECT \n" +
             "    COALESCE(SUM(distanceInMeters), 0) AS totalDistance,\n" +
